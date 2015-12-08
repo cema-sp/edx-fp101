9f508c988bfe7fe696b504a0acb4c629529a1de8
cema-sp <cema.rus@gmail.com>
2015-12-08 18:58:05 +0300

    Doing exercises from the book

--- 7_Functional_Parsers/Lab/chptr08.hs
+++ 7_Functional_Parsers/Lab/chptr08.hs
@@ -56,3 +56,120 @@ firstAndThird = do x <- item
 -- p +++ q = \inp -> case parse p inp of
 --                        []         -> parse q inp
 --                        [(v, out)] -> [(v, out)]
+
+-- sat :: (Char -> Bool) -> Parser Char
+-- sat p = do x <- item
+--            if p x then return x else failure
+
+-- many :: Parser a -> Parser [a]
+-- many p = many1 p +++ return []
+
+-- many1 :: Parser a -> Parser [a]
+-- many1 p = do v  <- p
+--              vs <- many p
+--              return (v:vs)
+
+listOfNaturals :: Parser [Int]
+listOfNaturals = do symbol "["
+                    n  <- natural
+                    ns <- many (do symbol ","
+                                   natural)
+                    symbol "]"
+                    return (n : ns)
+
+-- Grammar for arithmetic expression (WRONG)
+-- expr ::= expr '+' expr | expr '∗' expr | '('expr')' | nat
+-- nat ::= 0|1|2|···
+
+-- Grammar for a expression (VALID)
+-- expr ::= term '+' expr | term ::= term ('+' expr | -)
+-- term ::= factor '*' term | factor ::= factor ('*' factor | -)
+-- factor ::= '('expr')' | nat
+-- nat ::= 0|1|2|···
+
+-- expr :: Parser Int
+
+expr :: Parser Int
+expr = do t <- term
+          do symbol "+"
+             e <- expr
+             return (t + e)
+            +++
+            do symbol "-"
+               e <- expr
+               return (t - e)
+              +++
+              return t
+
+term :: Parser Int
+term = do e <- expo
+          do symbol "*"
+             t <- term
+             return (e * t)
+            +++
+            do symbol "/"
+               t <- term
+               return (e `div` t)
+              +++
+              return e
+
+expo :: Parser Int
+expo = do f <- factor
+          do symbol "^"
+             e <- expo
+             return (f ^ e)
+            +++
+            return f
+
+factor :: Parser Int
+factor = natural +++ do symbol "("
+                        e <- expr
+                        symbol ")"
+                        return e
+
+eval :: String -> Int
+eval xs = case parse expr xs of
+               [(n, [])]  -> n
+               [(_, out)] -> error ("unused input: " ++ out)
+               []         -> error "invalid input"
+
+int :: Parser Int
+int = natural +++ do symbol "-"
+                     n <- natural
+                     return (-n)
+
+integer :: Parser Int
+integer =  token int
+
+comment :: Parser ()
+comment = do symbol "--"
+             many (sat (/='\n'))
+             char '\n'
+             return ()
+
+-- Grammar for subtraction:
+-- subt ::= subt '-' subt | nat ::= subt '-' nat | nat ::= (subt '-' | -) nat
+-- nat ::= 0 | 1 | 2 | ...
+
+-- subt :: Parser Int
+-- subt = do s <- subt
+--           symbol "-"
+--           n <- natural
+--           return (s - n)
+--          +++
+--          natural
+
+-- ^^^^^^^^^^^^^^^^^^^^^^^^ - not working
+
+subt :: Parser Int
+subt = do n <- natural
+          ns <- many (do symbol "-"
+                         natural)
+          return (foldl (-) n ns)
+
+
+
+
+
+
+
