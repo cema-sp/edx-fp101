d3b47f12276df54f0e04e8f0515128cae30979f4
cema-sp <cema.rus@gmail.com>
2015-12-18 12:04:30 +0300

    Implementing exercises from the book

---
+++ 11_Lazy_Evaluation/Lab/chptr12.hs
@@ -0,0 +1,54 @@
+module Chptr12 where
+
+-- innermost evaluation === call-by-value (values passed)
+-- outermost evaluation === call-by-name (names passed (pointers ?))
+
+primes :: [Int]
+primes = sieve [2..]
+         where
+           sieve (p:ps) = p : sieve [ x | x <- ps
+                                        , x `mod` p /= 0 ]
+
+-- f $ x
+-- ^ - control part
+--     ^ - data part
+
+-- f $! x
+--   ^^ - strict evaluation (call-by-value), but it doesn't entirely evaluate
+
+sumwith :: Int -> [Int] -> Int
+--
+sumwith v []     = v
+-- sumwith v (x:xs) = sumwith (v + x) xs 
+-- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ - eats much memory
+sumwith v (x:xs) = (sumwith $! (v + x)) xs
+
+fibs :: [Integer]
+fibs = 0:1:[ x + x' | (x', x) <- zip fibs (tail fibs) ]
+
+fib :: Int -> Integer
+fib n = fibs !! n
+-- fib n = head [ x | (x, i) <- zip fibs [0..]
+--                  , i == n ]
+
+fibGT1000 :: Integer
+fibGT1000 = head . dropWhile (<1000) $ fibs
+-- fibGT1000 = head . filter (>1000) $ fibs
+
+
+data Tree a = Leaf | Node (Tree a) a (Tree a)
+              deriving (Show, Eq)
+
+repeatT :: Tree a -> Tree a
+repeatT Leaf         = error "Can not repeat Leaf"
+repeatT (Node l v r) = Node (repeatT n) v (repeatT n)
+                       where n = Node l v r
+
+takeT :: Int -> Tree a -> Tree a
+takeT 0 _            = Leaf
+takeT n Leaf         = Leaf
+takeT n (Node l v r) = Node (takeT (n - 1) t) v (takeT (n - 1) t)
+                       where t = Node l v r
+
+replicateT :: Int -> Tree a -> Tree a
+replicateT n = takeT n . repeatT
